410f77ea9e9efa559c6e5f4787dd26d00198af74
Serega55 <www.delphi7.ap55@gmail.com>
2024-02-20 13:57:00 +0300

    block count products

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
-    $('.fotorama').fotorama({
-        thumbsPreview: false,
+    $('.hero-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
         autoplay:true,
+        infinite: true,
+        pauseOnHover: false,
+        prevArrow: $('.btn-hero-sl_next'),
+        nextArrow: $('.btn-hero-sl_prev'),
     });
 });
