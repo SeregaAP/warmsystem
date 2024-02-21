e028efdbc2a4aa62adb1055042c29ee7c7ecbd55
Serega55 <www.delphi7.ap55@gmail.com>
2024-02-21 17:32:16 +0300

    add block project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    $(".fancybox").fancybox();
+    
     $('.hero-slider').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -7,5 +9,25 @@ $(document).ready(function(){
         pauseOnHover: false,
         prevArrow: $('.btn-hero-sl_next'),
         nextArrow: $('.btn-hero-sl_prev'),
+    });
+
+    $('.report__slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay:true,
+        infinite: true,
+        pauseOnHover: false,
+        prevArrow: $('.report-sl-back'),
+        nextArrow: $('.report-sl-next'),
+    });
+
+    $('.sertificate_lsider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay:true,
+        infinite: true,
+        pauseOnHover: false,
+        prevArrow: $('.sert-sl-back'),
+        nextArrow: $('.sert-sl-next'),
     });
 });
